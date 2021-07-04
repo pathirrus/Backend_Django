fe2b83b1995f77cb05e564d79ebb47fc13611a04
Patryq <p.kurc@o2.pl>
2021-07-04 14:23:01 +0200

    Django CR(UD)

--- hello/formapp4/models.py
+++ hello/formapp4/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     text = models.TextField()
+   # priority = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.text}"
--- hello/formapp4/views.py
+++ hello/formapp4/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+#from django.shortcuts import HttpResponseRedirect
+#from django.urls import reverse
+#
 
+from formapp4.models import Task
 # Create your views here.
 
 
 def register(request):
-    return render(
-        request,
-        'formapp4/register.html'
-    )
+
+    if request.method=="GET":
+        return render(
+            request,
+            'formapp4/register.html'
+        )
+    elif request.method=="POST":
+
+        task = request.POST.get('task')
+        if task:
+            task = Task(text=task)
+            task.save()
+        return redirect('formapp4:tasks_list')
+        #return HttpResponseRedirect(reverse('formapp4"task_list))
+
 
 def tasks_list(request):
+
+
+
+    tasks = Task.objects.all()
+
+
     return render(
         request,
         'formapp4/tasks_list.html',
